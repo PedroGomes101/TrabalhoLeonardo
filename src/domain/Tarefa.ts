26ba751bda255a09d9d0e3519519c1d1e276b615
Pedro Gomes D Carvalho Lopes <151851456+PedroGomes101@users.noreply.github.com>
2026-03-19 16:45:19 -0300

    retirada de comentarios

--- src/domain/Tarefa.ts
+++ src/domain/Tarefa.ts
@@ -26,42 +26,6 @@ class Tarefa{
         this.descricao = descricao;
         this.status = status;
     }
-    // get _nome(): string {
-    //     return this.nome;
-    // }
-    // get _descricao(): string {
-    //     return this.descricao;
-    // }
-    // get _status(): string {
-    //     return this.status;
-    // }
-    // set _nome(nome: string) {
-    //      if (!value || value.trim().length === 0) {
-    //         throw new Error("Nome não pode ser vazio");
-    //     }
-    //     if (value.length > 100) {
-    //         throw new Error("Nome não pode ter mais que 100 caracteres");
-    //     }
-    //     this.nome = nome;
-    // }
-    // set _descricao(descricao: string) {
-    //      if (!value || value.trim().length === 0) {
-    //         throw new Error("Nome não pode ser vazio");
-    //     }
-    //     if (value.length > 100) {
-    //         throw new Error("Nome não pode ter mais que 100 caracteres");
-    //     }
-    //     this.descricao = descricao;
-    // }
-    // set _status(status: string) {
-    //      if (!value || value.trim().length === 0) {
-    //         throw new Error("Nome não pode ser vazio");
-    //     }
-    //     if (value.length > 100) {
-    //         throw new Error("Nome não pode ter mais que 100 caracteres");
-    //     }
-    //     this.status = status;
-    // }
     toJson(){
         return {
             nome: this.nome,
